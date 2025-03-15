324edb7a60426cf48b36c72989e09d2fec5734d0
Jawid <jawid4mm@gmail.com>
2025-03-15 11:01:03 +0430

    listing first edit

--- src/pages/subPages/List.jsx
+++ src/pages/subPages/List.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import supabase from "../../SupabaseClient";
 
 function List() {
+
+  const [blogList, setBlogList] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data, error} = await supabase.from('BlogPosts').select();
+
+      if(error){
+        console.log(error)
+        setError(error)
+      }
+      else{
+        setBlogList(data)
+        setError(null)  
+        console.log(data)
+      }
+    }
+    fetchData()
+  }, [])
+
+  console.log(blogList)
+
   return (
-    <div>List</div>
+    <div>
+      
+    </div>
   )
 }
 
